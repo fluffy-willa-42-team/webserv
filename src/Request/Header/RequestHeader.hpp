2b48680436b68c972eb89d149c57419017073ab9
willaCS <arthur.willems.prive@gmail.com>
2023-05-02 09:28:01 +0000

    wip

--- src/Request/Header/RequestHeader.hpp
+++ src/Request/Header/RequestHeader.hpp
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 09:11:00 by awillems          #+#    #+#             */
-/*   Updated: 2023/05/02 09:14:16 by awillems         ###   ########.fr       */
+/*   Updated: 2023/05/02 09:25:00 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@
 
 class RequestHeader {
 	private:
-		std::string method;
-		std::string test;
+		std::string method		= "";	//	GET
+		std::string path		= "";	//	/test
+		std::string protocol	= "";	//	HTTP/1.1
+		std::string host		= "";	//	Host: www.example.com
+		std::string userAgent	= "";	//	User-Agent: Mozilla/5.0
+		std::string accept		= "";	//	Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+		std::string connection	= "";	//	Connection: keep-alive
 	protected:
 	public:
 		RequestHeader(){}
