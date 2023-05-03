40eb8427e9df439f81151cfedad13842a66a8901
willaCS <arthur.willems.prive@gmail.com>
2023-05-03 11:51:22 +0000

    simplifed operator<<

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:27:20 by awillems          #+#    #+#             */
-/*   Updated: 2023/05/03 11:41:06 by awillems         ###   ########.fr       */
+/*   Updated: 2023/05/03 11:50:58 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ std::ostream& operator<<(std::ostream& out, const Request& req){
 		out << "Invalid Request";
 		return out;
 	}
-	out << req.header.method << " " << req.header.path << " {" << endl;
-	out << "    header: {" << endl;
-	out << "        protocol: "		<< req.header.protocol	<< endl;
-	out << "        host: "			<< req.header.host	<< ":" << req.header.port << endl;
+	out << req.header.method << " " << req.header.path << " {" << endl
+	<< "    header: {" << endl
+	<< "        protocol: "		<< req.header.protocol	<< endl
+	<< "        host: "			<< req.header.host	<< ":" << req.header.port << endl;
 	for (map<string, string>::const_iterator ite = req.header.non_mandatory.begin(); ite != req.header.non_mandatory.end(); ite++){
 		out << "        " << ite->first << ": "	<< ite->second << endl;
 	}
-	out << "    }" << endl;
-	out << "    data: {";
+	out << "    }" << endl
+	<< "    data: {";
 	if (req.data.length() > 0){
 		out << endl << req.data << endl << "    ";
 	}
-	out << "}" << endl;
-	out << "}";
+	out << "}" << endl
+	<< "}";
 	return out;
 }
