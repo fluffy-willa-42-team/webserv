e790a41dfe853cff2dbd929ee436b33bf2a76735
willaCS <arthur.willems.prive@gmail.com>
2023-05-03 08:41:59 +0000

    ++

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ typedef struct unitests {
     const char* value;
 }	unitests;
 
-#define HEADER_TESTS_LENGTH 18
+#define HEADER_TESTS_LENGTH 22
 #define HEADER_TESTS {\
 	{ true,		"GET / HTTP/1.1\n"	},\
 	{ false,	"GET/ HTTP/1.1\n"	},\
@@ -34,6 +34,10 @@ typedef struct unitests {
 	{ false,	" / HTTP/1.1\n"		},\
 	{ false,	"/ HTTP/1.1\n"		},\
 	{ false,	" /HTTP/1.1\n"		},\
+	{ false,	""					},\
+	{ false,	"\n"				},\
+	{ false,	"    \n"			},\
+	{ false,	"xd\n"				},\
 }
 
 #define CORRECT_HEADERS_TEST "\
@@ -44,25 +48,34 @@ Accept-Language: en-GB,en;q=0.5\n\
 Accept-Encoding: gzip, deflate, br\n\
 Connection: keep-alive"
 
+void test(const string& value, unitests& unitest)
+{
+	if (VERBOSE_TEST)
+		cout << "\e[0;36m" << value << "\e[0m" << endl;
+
+	Request header(value);
+
+	if (VERBOSE_TEST){
+		cout << "Expected: " << (unitest.success_expected ? "true" : "false") << endl;
+		cout << "Result:   " << (header.isValid() ? "true" : "false") << endl;
+	}
+
+	if (header.isValid() == unitest.success_expected){
+		cout << "\e[0;32m" << "Success !" << "\e[0m" << endl;
+	}
+	else {
+		cout << "\e[0;31m" << "Failure !" << "\e[0m" << endl;
+	}
+
+	if (VERBOSE_TEST)
+		cout << endl;
+}
 
 int main(){
 	unitests unitests_headers[] = HEADER_TESTS;
 
-	for (int i = 0; i < HEADER_TESTS_LENGTH; i++){
-		string value = string() + unitests_headers[i].value + CORRECT_HEADERS_TEST;
-
-		if (VERBOSE_TEST)
-			cout << "\e[0;36m" << value << "\e[0m" << endl;
-
-		Request header(value);
-		if (header.isValid() == unitests_headers[i].success_expected){
-			cout << "\e[0;32m" << "Success !" << "\e[0m" << endl;
-		}
-		else {
-			cout << "\e[0;31m" << "Failure !" << "\e[0m" << endl;
-		}
-		if (VERBOSE_TEST)
-			cout << endl;
+	for (int i = 0; i < HEADER_TESTS_LENGTH; i++){// + CORRECT_HEADERS_TEST
+		test(string("") + unitests_headers[i].value, unitests_headers[i]);
 	}
 	return 0;
 }
