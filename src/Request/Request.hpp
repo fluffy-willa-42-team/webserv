c67f218269d4d21fe98a4fb911a5c7f55abc502e
willaCS <arthur.willems.prive@gmail.com>
2023-05-05 09:48:14 +0000

    ++

--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 09:11:05 by awillems          #+#    #+#             */
-/*   Updated: 2023/05/05 09:45:54 by awillems         ###   ########.fr       */
+/*   Updated: 2023/05/05 09:48:07 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@
 
 class Request {
 	private:
-		string data;
-		
 		bool valid;
 
 		string method;			//	GET
@@ -36,24 +34,18 @@ class Request {
 		size_t port;			//	8080
 
 		map<string, string> headers;
+		string data;
 
 		class InvalidRequest : public std::exception {
-			private:
-				string message;
 			public:
+				string message;
 				InvalidRequest(): message("Invalid Request"){}
-				InvalidRequest(const string& message): message("Invalid Request"){
-					this->message += ": " + message;
-				}
+				InvalidRequest(const string& message): message("Invalid Request"){ this->message += ": " + message; }
 				~InvalidRequest() throw() {}
-				virtual const char* what() const throw() {
-					return this->message.c_str();
-				}
+				virtual const char* what() const throw() { return this->message.c_str(); }
 		};
 		
 	public:
-
-	
 		Request(const std::string& input);
 
 		bool isValid(){ return this->valid; }
