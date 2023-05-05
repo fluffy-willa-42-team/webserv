0f57b0b782c517f01a3e817710325642a2ea9678
willaCS <arthur.willems.prive@gmail.com>
2023-05-05 09:48:37 +0000

    ++

--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 09:11:05 by awillems          #+#    #+#             */
-/*   Updated: 2023/05/05 09:48:07 by awillems         ###   ########.fr       */
+/*   Updated: 2023/05/05 09:48:31 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,8 @@ class Request {
 		class InvalidRequest : public std::exception {
 			public:
 				string message;
-				InvalidRequest(): message("Invalid Request"){}
-				InvalidRequest(const string& message): message("Invalid Request"){ this->message += ": " + message; }
+				InvalidRequest(): 						message("Invalid Request"){}
+				InvalidRequest(const string& message): 	message("Invalid Request"){ this->message += ": " + message; }
 				~InvalidRequest() throw() {}
 				virtual const char* what() const throw() { return this->message.c_str(); }
 		};
