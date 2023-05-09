4ccacb31cb5ceedea4572e4d27162585d3bde107
willaCS <arthur.willems.prive@gmail.com>
2023-05-09 12:20:27 +0000

    added tmp Delete Method

---
+++ src/Response/Methods/Delete/Delete.hpp
@@ -0,0 +1,27 @@
+#ifndef DELETE_HPP
+# define DELETE_HPP
+
+/* ************************************************************************** */
+
+#include <iostream>
+#include <Response.hpp>
+
+/* ************************************************************************** */
+
+class Delete : private Response {
+	private:
+	protected:
+
+	public:
+		Delete(Request req) : Response(req){
+			cout << PURPLE << "Constructing a Delete method" << RESET << endl;
+		}
+
+		string toString(){
+			return "";
+		}
+};
+
+/* ************************************************************************** */
+
+#endif /* DELETE_HPP */
