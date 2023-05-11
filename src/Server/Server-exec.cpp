0e7283648b06d2e96d07ff90d5c9067c1c8e66a5
willaCS <arthur.willems.prive@gmail.com>
2023-05-11 12:22:59 +0000

    ++

--- src/Server/Server-exec.cpp
+++ src/Server/Server-exec.cpp
@@ -2,18 +2,6 @@
 #include "Request.hpp"
 #include "Response.hpp"
 
-#define TEST_MESSAGE "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: "
-
-string get_message_test(const Request &req){
-	stringstream ss1;
-	ss1 << req;
-
-	stringstream ss;
-    ss << TEST_MESSAGE << ss1.str().length() << "\n\n" << ss1.str();
-    string message = ss.str();
-	return message;
-}
-
 void Server::exec(){
 	read(connection_fd, buffer, BUFFER_SIZE);
 	cout << buffer << endl;
@@ -22,7 +10,7 @@ void Server::exec(){
 
 	Response* res = response_factory(req, config);
 	if (!res)
-		cout << "EREREROOOR" << endl;
+		throw runtime_error("response is null");
 
 	string message = res->toString();
 	write(connection_fd , message.c_str(), message.length());
