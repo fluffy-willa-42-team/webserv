f89024f85875b59e45fe584f23fc6dd9351c8c51
willaCS <arthur.willems.prive@gmail.com>
2023-09-04 09:28:34 +0000

    reset

--- src/Response/Content/Content.cpp
+++
@@ -1,9 +0,0 @@
-#include "Content.hpp"
-
-Content::~Content(){}
-
-std::ostream& operator<<(std::ostream& out, const Content& content)
-{
-	out << content.toString();
-	return out;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,8 @@
-#include "Request.hpp"
-#include "Server.hpp"
 #include "webserv.hpp"
 
-#include <csignal>
-
-bool loop = true;
-map<int, Server> servers;
-
-int start_test();
-void test_req();
-
-void start(){
-	for (map<int, Server>::iterator ite = servers.begin(); ite != servers.end(); ite++){
-		try {
-			ite->second.setup();
-			ite->second.print_waiting_msg();
-		}
-		catch(const std::exception& e) {
-			std::cerr << RED << e.what() << RESET << endl;
-		}
-		
-	}
-	while (loop){
-		for (map<int, Server>::iterator ite = servers.begin(); ite != servers.end(); ite++){
-			if (ite->second.is_it_running())
-				ite->second.try_connection();
-		}
-	}
-}
-
-void shutdown(int signal){
-	(void) signal;
-	for (map<int, Server>::iterator ite = servers.begin(); ite != servers.end(); ite++){
-		ite->second.stop();
-	}
-	loop = false;
-}
+// map<int, Server> servers;
 
 void webserv(){
-	// start_test();
-	// test_req();
-
-	//TODO load config file
-
-	//TODO setup Server with config
-	servers[8001] = Server(Address("0.0.0.0", "8001"));
-	servers[8002] = Server(Address("0.0.0.0", "8002"));
-	servers[8003] = Server(Address("0.0.0.0", "8003"));
-
-	std::signal(SIGINT, &shutdown);
-	start();
 }
 
 int main(){
