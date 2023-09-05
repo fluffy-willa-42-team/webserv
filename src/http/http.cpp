a36bb6a1882af557d2407c2443ee614e016d0336
willaCS <arthur.willems.prive@gmail.com>
2023-09-05 13:46:50 +0000

    added todo

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -5,6 +5,7 @@
 string error(u_int32_t code, const string& message);
 
 const string http(const string& req){
-	// Parse_request()
+	// Parse_request() => return a struct or a class for a request with : path header protocol body 
+	// Try and execute the right content for the request (CGI file ou redirection)
 	return error(404, "This Page has not been Found");
 }
