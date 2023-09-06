5ce35164207e5428da9e8314fa9a442369ca6a7e
willaCS <arthur.willems.prive@gmail.com>
2023-09-06 09:52:26 +0000

    updated doc for http

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -8,16 +8,36 @@ const string http(const string& req){
 	std::istringstream inputStream(req);
     std::string line;
 
-	/*===-----		Init Line		-----===*/
+	/*===-----						Init Line						   -----===*
+	METHOD PATH+PARAMS PROTOCOL
+
+	Method: GET | POST | PATCH | DELETE | ...
+	Path + Params: /test | / | /hello/world?query=true
+	Protocol: HTTP/1.1
+
+	Ex:
+	GET /test?query=true HTTP/1.1
+	*/
+
 	if (!std::getline(inputStream, line)){
 		return error(400, "The Request is empty");
 	}
 	std::cout << "Init Line: " << line << std::endl;
 
 
+	/*===-----						Host Header						   -----===*
+	Host: HOSTNAME:PORT
 
-	/*===-----		Host Header		-----===*/
-	map<string, string> req_headers;
+	Hostname: localhost | awillems.42.fr
+	Port (optional): 8000 | 4000 | ...
+
+	Ex:
+	Host: awillems.42.fr:4000
+	*/
+
+	string req_host;
+	u_int32_t req_port;
+
 	if (!std::getline(inputStream, line)){
 		return error(400, "The host header is missing");
 	}
@@ -25,8 +45,13 @@ const string http(const string& req){
 
 
 
-	/*===-----		Headers			-----===*/
-	
+	/*===-----						Headers							   -----===*
+	HEADER_KEY: HEADER_VALUE
+
+	Ex:
+	Accept-Language: en-US,en
+	*/
+	map<string, string> req_headers;
     while (std::getline(inputStream, line) && line != "\r") {
         std::cout << "Header Line: " << line << std::endl;
     }
