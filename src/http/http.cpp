3bdca863302541cd068dc9d308e1916d41e1e24c
willaCS <arthur.willems.prive@gmail.com>
2023-09-06 09:53:07 +0000

    ++

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -56,34 +56,7 @@ const string http(const string& req){
         std::cout << "Header Line: " << line << std::endl;
     }
 
-
+	/*===-----						Body							  -----===*/
 
 	return error(404, "This Page has not been Found");
 }
-
-
-
-
-
-
-/*
-
-GET / HTTP/1.1
-Host: localhost:8001
-Connection: keep-alive
-Cache-Control: max-age=0
-sec-ch-ua: "Brave";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
-sec-ch-ua-mobile: ?0
-sec-ch-ua-platform: "macOS"
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,;q=0.8
-Sec-GPC: 1
-Accept-Language: en-US,en
-Sec-Fetch-Site: none
-Sec-Fetch-Mode: navigate
-Sec-Fetch-User: ?1
-Sec-Fetch-Dest: document
-Accept-Encoding: gzip, deflate, br
-
-*/
