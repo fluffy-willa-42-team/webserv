adb7777a8d7edd444fa5469471f4c7d865c2dd08
willaCS <arthur.willems.prive@gmail.com>
2023-09-19 12:04:28 +0200

    ++

--- src/Config/Config-parse.cpp
+++ src/Config/Config-parse.cpp
@@ -1,19 +1,5 @@
 #include "Config.hpp"
-
-e_status parseline(ifstream& file, string& line, vector<string>& line_split, e_status& status, u_int32_t& index);
-bool is_server_line							(vector<string>& line_split);
-bool is_location_line						(vector<string>& line_split);
-bool is_server_option_server_name			(vector<string>& line_split);
-bool is_server_option_listen				(vector<string>& line_split);
-bool is_server_option_error_page			(vector<string>& line_split);
-bool is_server_option_max_client_body_size	(vector<string>& line_split);
-bool is_location_index						(vector<string>& line_split);
-bool is_location_root						(vector<string>& line_split);
-bool is_location_allow_methods				(vector<string>& line_split);
-bool is_location_return						(vector<string>& line_split);
-bool is_location_cgi_pass					(vector<string>& line_split);
-bool is_location_download_file				(vector<string>& line_split);
-bool is_location_autoindex					(vector<string>& line_split);
+#include "file_parsing.hpp"
 
 static e_status err(const string& line, const u_int32_t& index){
 	cerr << RED << "[" << index << "] \"" << line << "\"" << RESET << endl;
