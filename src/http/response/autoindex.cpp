90c43e1fde6585a5274d01275942a97b48a354e4
willaCS <arthur.willems.prive@gmail.com>
2023-09-20 17:23:48 +0200

    moved html files

---
+++ src/http/response/autoindex.cpp
@@ -0,0 +1,7 @@
+#include "webserv.hpp"
+#include "utils.hpp"
+
+static const string autoindex_fragment_file = readFileIntoString("./src/html/autoindex_autoindex.html");
+static const string autoindex_file = readFileIntoString("./src/html/autoindex.html");
+
+
