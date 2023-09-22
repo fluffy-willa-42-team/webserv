a14f1824f55869b4704e93594a84e0ee848db8c1
willaCS <arthur.willems.prive@gmail.com>
2023-09-22 12:40:09 +0200

    fixed missing debug endl

--- src/http/response/redirect.cpp
+++ src/http/response/redirect.cpp
@@ -24,6 +24,5 @@ bool is_valid_redirect_code(u_int32_t code){
 string redirect(u_int32_t code, const string& path){
 	map<string, string> header;
 	header[HEADER_REDIRECT] = path;
-	cout << code << endl;
 	return get_response(code, header);
 }
