96362035ca365d1c1b0ecccd5773a6e238c94648
willaCS <arthur.willems.prive@gmail.com>
2023-10-09 11:12:28 +0200

    wip example2

---
+++ static/example2/index.pl
@@ -0,0 +1,33 @@
+use CGI;
+
+my $cgi = CGI->new;
+my $message;
+if ($ENV{'REDIRECT_STATUS'} eq '200'){
+	$message = 'This is a Perl generated message for GET';
+} else {
+	$message = 'This is a Perl generated message for redirect';
+}
+
+print $cgi->header;
+print <<HTML;
+<html>
+	<head>
+		<title>Perl index</title>
+	</head>
+	<body>
+		<h1>Perl index</h1>
+		<h2>$message</h2>
+		<form action="/post" method="post" enctype="multipart/form-data">
+			<label for="hello">Drop file</label>
+			<input type="file" id="hello" name="fileToUpload">
+
+			<input type="submit" value="submit">
+		</form>
+		<form action="/delete" method="delete">
+			<label for="filename">Enter the filename to delete: </label>
+			<input type="text" name="filename">
+			<input type="submit" value="Delete">
+		</form>
+	</body>
+</html>
+HTML
