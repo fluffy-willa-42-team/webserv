e9b0c78ba0f730e06181edf50b06e2630b522054
willaCS <arthur.willems.prive@gmail.com>
2023-10-11 14:40:08 +0200

    cleanup

--- src/http/response/specifics/cgi/1_execute_cgi.cpp
+++ src/http/response/specifics/cgi/1_execute_cgi.cpp
@@ -49,8 +49,8 @@ e_status exec_cgi(
 	const string& file,
 	string& response
 ){
-	t_pipe pipe1 = {-1, -1};
-	t_pipe pipe2 = {-1, -1};
+	t_pipe pipe_out = {-1, -1};
+	t_pipe pipe_in = {-1, -1};
 	char * const argv[3] = {
 		const_cast<char*>(cgi_bin.c_str()),
 		const_cast<char*>(file.c_str()),
@@ -59,41 +59,32 @@ e_status exec_cgi(
 	char * const *env_cast = createCopy(env);
 
 	if (!env_cast){
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		DEBUG_ERROR_ << "CGI: Could not create envirement variable" << endl;
 		return S_ERROR;
 	}
 
-	if (pipe((int *) &pipe1) < 0){
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+	if (pipe((int *) &pipe_out) < 0){
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		DEBUG_ERROR_ << "CGI: Could not create a pipe" << endl;
 		return S_ERROR;
 	}
-	if (pipe((int *) &pipe2) < 0){
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+	if (pipe((int *) &pipe_in) < 0){
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		DEBUG_ERROR_ << "CGI: Could not create a pipe" << endl;
 		return S_ERROR;
 	}
 
 	pid_t pid = fork();
 	if (pid < 0){
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		DEBUG_ERROR_ << "CGI: Could not Fork" << endl;
 		return S_ERROR;
 	}
 	else if (pid == 0){
-		if (dup2(pipe2.read, STDIN_FILENO) < 0){
-			free_exec_cgi(env, env_cast, pipe1, pipe2);
-			exit(EXIT_FAILURE);
-		}
-		if (dup2(pipe1.write, STDOUT_FILENO) < 0){
-			free_exec_cgi(env, env_cast, pipe1, pipe2);
-			exit(EXIT_FAILURE);
-		}
-		
 		if (!req_body.empty()){
-			DEBUG_ERROR_ << "Has Body: " << req_body.size() << endl;
-			ssize_t write_c = write(pipe2.write, req_body.c_str(), req_body.size());
+			DEBUG_ERROR_ << "Has Body: " << req_body.size() << " | " << endl;
+			ssize_t write_c = write(pipe_in.write, req_body.c_str(), req_body.size());
 			if (write_c == -1){
 				DEBUG_ERROR_ << "CGI write fail " << strerror(errno) << endl;
 				exit(EXIT_FAILURE);
@@ -102,7 +93,7 @@ e_status exec_cgi(
 		}
 		else {
 			DEBUG_ERROR_ << "Has No Body" << endl;
-			ssize_t write_c = write(pipe2.write, "\0", 1);
+			ssize_t write_c = write(pipe_in.write, "\0", 1);
 			if (write_c == -1){
 				DEBUG_ERROR_ << "CGI write fail " << strerror(errno) << endl;
 				exit(EXIT_FAILURE);
@@ -110,51 +101,50 @@ e_status exec_cgi(
 		}
 
 		DEBUG_ERROR_ << "execve ee" << endl;
+		
+		close(pipe_in.write);
+		close(pipe_out.read);
 
-		close(pipe1.write);
-		close(pipe1.read);
-		close(pipe2.write);
-		close(pipe2.read);
+		if (dup2(pipe_in.read, STDIN_FILENO) < 0){
+			free_exec_cgi(env, env_cast, pipe_out, pipe_in);
+			exit(EXIT_FAILURE);
+		}
+		if (dup2(pipe_out.write, STDOUT_FILENO) < 0){
+			free_exec_cgi(env, env_cast, pipe_out, pipe_in);
+			exit(EXIT_FAILURE);
+		}
+
+		close(pipe_out.write);
+		close(pipe_in.read);
 
 		DEBUG_ERROR_ << "execve" << endl;
 		execve(argv[0], argv, env_cast);
 		DEBUG_ERROR_ << "Failed execve" << endl;
 
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		exit(EXIT_FAILURE);
 	}
 	else {
-		close(pipe2.read);
-		close(pipe2.write);
-		close(pipe1.write);
+		close(pipe_in.read);
+		close(pipe_in.write);
+		close(pipe_out.write);
 
 		int child_status = 0;
 		waitpid(pid, &child_status, 0);
 
 		DEBUG_ERROR_ << "End execve" << endl;
 
-		int flags = fcntl(pipe1.read, F_GETFL, 0);
-		if (flags == -1) {
-			DEBUG_ERROR_ << "CGI: Could not get flag for pipe" << endl;
-			return S_ERROR;
-		}
-
-		if (fcntl(pipe1.read, F_SETFL, flags | O_NONBLOCK) == -1) {
-			DEBUG_ERROR_ << "CGI: Could not set flag for pipe" << endl;
-			return S_ERROR;
-		}
-		
 		if (!WIFEXITED(child_status)){
-			free_exec_cgi(env, env_cast, pipe1, pipe2);
+			free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 			DEBUG_ERROR_ << "CGI: Did not execute properly" << endl;
 			return S_ERROR;
 		}
 
 		e_status r_status = S_CONTINUE;
 		while (r_status == S_CONTINUE){
-			response += read_buff_cgi(pipe1.read, r_status);
+			response += read_buff_cgi(pipe_out.read, r_status);
 			if (r_status == S_ERROR){
-				free_exec_cgi(env, env_cast, pipe1, pipe2);
+				free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 				DEBUG_ERROR_ << "CGI: Could not Read" << endl;
 				return S_ERROR;
 			}
@@ -162,7 +152,7 @@ e_status exec_cgi(
 
 		DEBUG_ << "CGI response: " << endl << BLUE << response << RESET << endl;
 		
-		free_exec_cgi(env, env_cast, pipe1, pipe2);
+		free_exec_cgi(env, env_cast, pipe_out, pipe_in);
 		return S_CONTINUE;
 	}
 }
